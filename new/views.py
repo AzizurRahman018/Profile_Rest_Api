0d7e3b90cb88a1744c918eef37656f5f17d2b326
Md Azizur Rahman <106616550+AzizurRahman018@users.noreply.github.com>
2026-03-15 21:38:23 +0600

    api

--- new/views.py
+++ new/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framwork.veiws import APIView
+from rest_framwork.response import Response
 # Create your views here.
+class HelloapiView(APIView):
+    """
+    This is the api view for hello api
+    """
